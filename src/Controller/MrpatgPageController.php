28c1a2830bb2ba6e761cea7c120bea3450ceb706
Patrick Godbey <mrpatg@gmail.com>
2017-06-13 09:12:12 -0400

    got the list of pages

--- src/Controller/MrpatgPageController.php
+++ src/Controller/MrpatgPageController.php
@@ -7,9 +7,26 @@ use Drupal\Core\Controller\ControllerBase;
 class MrpatgPageController extends ControllerBase {
 
     public function mrpatgPage1() {
-        $element = array(
-            '#markup' => 'Hello, world',
-        );
-        return $element;
+
+        $values = [
+        'type' => 'page'
+        ];
+        $nodes = \Drupal::entityTypeManager()->getListBuilder('node')->getStorage()->loadByProperties($values);
+        
+        $list = array();
+
+        foreach ($nodes AS $node){
+            $list[] = $node->get('title')->value;
+        }
+
+        $content = [
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#title' => 'Page list',
+        '#items' => $list,
+        '#attributes' => ['class' => 'mrpatg_page1_list'],
+        '#wrapper_attributes' => ['class' => 'container'],
+        ]; 
+        return $content;
     }
 }
